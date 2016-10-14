585e0684d6740b4d31cd593267afdde95ea08374
dooully <dooully@gmail.com>
2016-10-14 13:51:47 +0900

    샘플 Unit Id 설정

--- sample/exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
+++ sample/exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
@@ -6,9 +6,11 @@ package com.onnuridmc.sample;
 public class AppConstants {
     public static final boolean TEST_MODE = true;
 
-    public static final String UNIT_ID_BANNER = "{BANNER}";
-    public static final String UNIT_ID_INTERSTITIAL = "{INTERSTITIAL}";
-    public static final String UNIT_ID_NATIVE = "{NATIVE}";
+    // AndroidManifast.xml에 AppId를 먼저 설정해야 합니다.
+    // Unit아이디를 설정합니다.
+    public static final String UNIT_ID_BANNER = "e81b29704e65d960528dbb93f388d1174d186fe8";
+    public static final String UNIT_ID_INTERSTITIAL = "b7cbfa96e2b0ca2b23c98af34e4ff35f18d5f4cf";
+    public static final String UNIT_ID_NATIVE = "3cef64be38845c4fcdbc2313579d30f2d1f8453a";
 
 
 }
