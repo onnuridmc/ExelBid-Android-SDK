3ef3f3915556c32821886c90893a52abf7b4891b
Son <hungryson@naver.com>
2017-05-19 10:47:46 +0900

    compile 환경, gradle 환경 업데이트

--- sample/exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
+++ sample/exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
@@ -4,13 +4,13 @@ package com.onnuridmc.sample;
  * Created by Administrator on 2016-09-01.
  */
 public class AppConstants {
-    public static final boolean TEST_MODE = true;
+    public static final boolean TEST_MODE = false;
 
     // AndroidManifast.xml에 AppId를 먼저 설정해야 합니다.
     // Unit아이디를 설정합니다.
     public static final String UNIT_ID_BANNER = "e81b29704e65d960528dbb93f388d1174d186fe8";
     public static final String UNIT_ID_INTERSTITIAL = "b7cbfa96e2b0ca2b23c98af34e4ff35f18d5f4cf";
-    public static final String UNIT_ID_NATIVE = "3cef64be38845c4fcdbc2313579d30f2d1f8453a";
+    public static final String UNIT_ID_NATIVE = "a3b5f3795524714e630e26311591022cef03180b";
 
 
 }
