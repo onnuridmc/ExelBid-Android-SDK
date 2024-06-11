a79f30661d71d949e2f90f09641e08e610b0fc1c
kyunglim <kyunglim.kang@motiv-i.com>
2024-06-11 18:27:28 +0900

    Update : 샘플앱 unitId 수정

--- exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
+++ exelbid-sample/src/main/java/com/onnuridmc/sample/AppConstants.java
@@ -7,8 +7,8 @@ public class AppConstants {
     public static final boolean TEST_MODE = false;
 
 
-    public static final String UNIT_ID_BANNER = "e81b29704e65d960528dbb93f388d1174d186fe8";
-    public static final String UNIT_ID_INTERSTITIAL = "b7cbfa96e2b0ca2b23c98af34e4ff35f18d5f4cf";
-    public static final String UNIT_ID_NATIVE = "3cef64be38845c4fcdbc2313579d30f2d1f8453a";
+    public static final String UNIT_ID_BANNER = "fb59bdd7ffd4e9868e0d6dde98445a8854882a28";
+    public static final String UNIT_ID_INTERSTITIAL = "f028a0c209dfc9fe7243381f3ba92eabe640cfbf";
+    public static final String UNIT_ID_NATIVE = "b014ec22dccb43bfc35ea1e5051c83862d7357cf";
 
 }
